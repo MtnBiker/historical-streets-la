d63c949540338d6d35d31af72b400b26571e782f
Greg Scarich <gscar@Gregs-MacBook-Pro.local>
2017-06-03 13:51:31 -0700

    Added some static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/acknowledgements'
+
   root 'streets#index'
   resources :streets
   # get 'jqgrid'  => 'streets#jqgrid'
