5074ba25d117c0edaaf7b820e5d0d2a259acc1fa
Greg Scarich <git@web.knobby.ws>
2017-06-06 12:02:53 -0700

    Styling for debug dump from Hartl
    Working on getting mailer to work and ran across this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   #   devise_parameter_sanitizer.for(:sign_up) << :name
   #   devise_parameter_sanitizer.for(:account_update) << :name
   # end
-  
+
   # Similar from https://github.com/plataformatec/devise#getting-started
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -20,6 +20,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
-  
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable,
          :omniauthable, :omniauth_providers => [:facebook]
+
+# Below and line just above from https://revs.runtime-revolution.com/working-with-facebook-using-devise-omniauth-koala-and-rails-5-cde5d032de02
+  def self.create_from_omniauth(params)
+    user = find_or_create_by(email: params.info.email, uid: params.uid)
+    user.update({
+      token: params.credentials.token,
+      name: params.info.name,
+      avatar: params.info.image
+    })
+    user
+  end
 end
