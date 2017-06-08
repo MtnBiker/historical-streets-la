466978c4b1407546aa1b1e02d91e785e85fd3966
Greg Scarich <git@web.knobby.ws>
2017-06-08 10:29:54 -0700

    Messed with testing 5.3.4
    Mostly not working. TODO

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,14 +2,15 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
-  # full_title "layout links" do
-  #   get root_path
-  #   # assert_template 'streets#index'
-  #   assert_select "a[href=?]", root_path, count: 0 # see Hartl, no idea what this is about
-  #   # assert_select "a[href=?]", help_path
-  #   # assert_select "a[href=?]", about_path
-  #   # assert_select "a[href=?]", acknowlegements_path
-  #   # assert_select "title", full_title("") # undefined method `full_title' for SiteLayoutTest:Class (NoMethodError). From Hartl
-  # end
+  test "layout links" do
+    get root_path
+    # assert_template 'streets#index'
+    assert_template 'static_pages/home'
+    # assert_select "a[href=?]", root_path # , count: 2 # see Hartl, no idea what this is about
+    # assert_select "a[href=?]", help_path
+    # assert_select "a[href=?]", about_path
+    # assert_select "a[href=?]", acknowlegements_path
+    # assert_select "title", full_title("") # <Historical Street Names in Los Angeles> expected but was         <SyntaxError at />..
+  end
 
 end
