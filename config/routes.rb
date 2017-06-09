6bb768ec183cd6cef57b629615b78828ad100ca0
Greg Scarich <git@web.knobby.ws>
2017-06-08 19:35:44 -0700

    Hartl 8.1 Login part a
    But something wrong with my submit on login

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   get  '/acknowledgements',   to: 'static_pages#acknowledgements'
   get 'about' => 'static_pages#about'
   get 'acknowledgments' => 'static_pages#acknowledgments'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
