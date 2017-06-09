6c5a218820acb2ccf10420c9c3318a9dfa7ff39a
Greg Scarich <git@web.knobby.ws>
2017-06-09 11:18:15 -0700

    Hartl 9.3.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,26 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper # Hartl doesn't have this. Is it by default or did I put it in?
 
-  # Returns true if a test user is logged in. Hartl Listing 8.26
+  # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
