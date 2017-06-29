276639ee20ce2cf0c329ac07be7627c4c061d6af
Greg Scarich <git@web.knobby.ws>
2017-06-29 11:38:15 -0700

    jsLint clean up

--- app/assets/javascripts/ie10-viewport-bug-workaround.js
+++ app/assets/javascripts/ie10-viewport-bug-workaround.js
@@ -7,18 +7,18 @@
 
 // See the Getting Started docs for more information:
 // https://getbootstrap.com/getting-started/#support-ie10-width
-
+"use strict";
 (function () {
-  'use strict'
+
 
   if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
+    var msViewportStyle = document.createElement("style");
     msViewportStyle.appendChild(
       document.createTextNode(
-        '@-ms-viewport{width:auto!important}'
+        "@-ms-viewport{width:auto!important}"
       )
     )
-    document.head.appendChild(msViewportStyle)
+    document.head.appendChild(msViewportStyle);
   }
 
-}())
+}());
