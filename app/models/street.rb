de78ebac048fbed987a4ecd362a55e185c1e0630
Greg Scarich <git@web.knobby.ws>
2018-01-06 14:13:20 -0800

    Changed where to cross_streets
    Works

--- app/models/street.rb
+++ app/models/street.rb
@@ -31,7 +31,7 @@ class Street < ApplicationRecord
   #       # failed when tried "%#{search}%" or '%#{search}%' can put space around between the #{} but then the space is there and don't know if that messes up SQL
   #       # More complicated syntax is needed for camelCase fields. But seems to be needed for all fields. If use shorter syntax those fields are missed--not sure what's going on ref1, ref2 and ref3 don't seem to be being searched. Ahh, only searching on the last field in the list
   #   # self.where('"streets"."previous_name" ILIKE ?' OR '"streets"."current_name" ILIKE ?', search, search) # syntax error, unexpected tCONSTANT, expecting ')' treets"."previous_name" ILIKE ?' OR '"streets"."current_name" ILIK
-  #   self.where('"streets"."where" ILIKE ?', search); # this does work
+  #   self.where('"streets"."cross_streets" ILIKE ?', search); # this does work
   # end
   
 end
