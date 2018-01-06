000a9df47c5e005bbfd2f84ea216fe52399cbed3
Greg Scarich <git@web.knobby.ws>
2018-01-06 14:18:32 -0800

    Changed search and seems to work
    Maps may not be working
    
    But search is

--- app/models/street.rb
+++ app/models/street.rb
@@ -22,7 +22,7 @@ class Street < ApplicationRecord
   # end
   
   def self.search(search)
-    where("ref1 ILIKE ? OR ref2 ILIKE ? OR ref3 ILIKE ? OR notes ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")     
+    where("previous_name ILIKE ? OR current_name ILIKE ? OR date_earliest ILIKE ? OR date_latest ILIKE ? OR cross_streets ILIKE ? OR ref1 ILIKE ? OR ref2 ILIKE ? OR ref3 ILIKE ? OR notes ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")     
   end
 
 # Trying a different concatenation of multiple. Didn't work
