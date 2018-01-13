beae010f3f5c1d5531da03a5d0983d8c74fe88b1
Greg Scarich <git@web.knobby.ws>
2018-01-13 09:02:58 -0800

    Added Show All back to streets>index
    And other edits
    
    And trying to fix button appearance on overlay_selector

--- app/models/street.rb
+++ app/models/street.rb
@@ -3,8 +3,13 @@ class Street < ApplicationRecord
   has_paper_trail
   validates :previous_name, :current_name, presence: true
 
+# Used by search on streets>index. Maybe came from here https://stackoverflow.com/questions/39685055/self-search-body-rails
   def self.search(search)
     where("previous_name ILIKE ? OR current_name ILIKE ? OR date_earliest ILIKE ? OR date_latest ILIKE ? OR cross_streets ILIKE ? OR ref1 ILIKE ? OR ref2 ILIKE ? OR ref3 ILIKE ? OR notes ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")     
+  end
+
+  def json_to_geojson
+    
   end
 
 end
