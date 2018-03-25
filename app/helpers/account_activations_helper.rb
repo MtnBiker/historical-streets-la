45220472067cf126508002e9fb5faa0467407595
Greg Scarich <git@web.knobby.ws>
2018-03-24 17:43:46 -0700

    Can't see users at heroku
    Tried removing one fatal error?
    
    Also changed menu a bit for logical readability

--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -1,2 +1,6 @@
 module AccountActivationsHelper
 end
+
+# I put in the following Listings and got some errors TODO ?
+# In other words probably needed to do some other things
+# Listing 11.31 and 11.37 Hartl Rails 5
